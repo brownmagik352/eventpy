3070c1921ae59666ef7ca86e84e4df7ddc08a89f
brownmagik352 <sumanicus@gmail.com>
2014-07-25 11:25:33 -0400

    added event model, basic setup

---
+++ eventbook/events/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Event(models.Model):
+	title = models.CharField(max_length = 100)
+	location = models.CharField(max_length=50)
+	time = models.DateTimeField(auto_now_add=True)
+	description =models.TextField(max_length=500)
+
+	def __unicode__(self):
+		return self.title
+
+	def isEXEvent(self):
+		return (self.location == "33 Lynwood")
