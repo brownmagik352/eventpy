72aee1e1f638e137d806677acb71e572c3d78862
brownmagik352 <sumanicus@gmail.com>
2014-07-26 18:52:40 -0400

    pull individual events from url, second view

--- eventbook/events/views.py
+++ eventbook/events/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from models import Event
 
 def index(request):
-	return HttpResponse("Hello world!")
+	message = ""
+	for e in Event.objects.all():
+		message += "%s\t" % e.title
+	return HttpResponse("All Events:\t" + message)
+
+def detail(request, event_id):
+	e = Event.objects.get(pk=event_id)
+	return HttpResponse("%s" % e.title)
