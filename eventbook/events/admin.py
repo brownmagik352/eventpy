618e3f1d10fdb79c12b48485f4182e69e210dc4d
brownmagik352 <sumanicus@gmail.com>
2014-07-29 15:16:09 -0400

    took out group table

--- eventbook/events/admin.py
+++ eventbook/events/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from events.models import Event, Group
+from events.models import Event
 
-admin.site.register(Group)
 admin.site.register(Event)
--- eventbook/events/models.py
+++ eventbook/events/models.py
@@ -1,16 +1,8 @@
 from django.db import models
 from datetime import datetime
 
-class Group(models.Model):
-	name = models.CharField(max_length=75, unique=True)
-	numMembers = models.IntegerField(default=0)
-	private = models.BooleanField(default=False)
-
-	def __unicode__(self):
-		return self.name
 
 class Event(models.Model):
-	group = models.ForeignKey(Group, null=True)
 	title = models.CharField(max_length = 100, unique = True)
 	location = models.CharField(max_length=50)
 	time = models.DateTimeField(default=datetime.now())
